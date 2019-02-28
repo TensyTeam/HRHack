cceba9d5dddea89f3b2e2b0c935434d9f49690bb
Mike Petrov <mis5116@yandex.ru>
2019-03-01 01:44:43 +0300

    profile page

---
+++ src/Panels/UserProfile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { arrayCards } from './ArrayCards';
+
+class UserProfile extends React.Component {
+
+	render() {
+		return (
+			<div id="UserProfile">
+				<div className="title">HR 2.0</div>
+				<div className="container">
+					<button onClick={() => { this.props.onUpdatePanel('CardList')}} className="btn">Назад</button>
+					<div className="content-profile">
+						<img src={arrayCards[this.props.idUser].image} className="card-image"/>
+						<div className="card-info">
+							<div className="card-name">{arrayCards[this.props.idUser].name + ' ' + arrayCards[this.props.idUser].surname}</div>
+							<div className="card-phone">{arrayCards[this.props.idUser].phone}</div>
+							<div className="card-mail">{arrayCards[this.props.idUser].mail}</div>
+						</div>
+						<button className="btn">Связаться</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+export default UserProfile;
