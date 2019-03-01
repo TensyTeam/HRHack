e6fbd458cb1a09a8ef3931cc60429212b2d1e097
Mike Petrov <mis5116@yandex.ru>
2019-03-01 04:11:33 +0300

    new panel user profile

--- src/Panels/UserProfile.js
+++ src/Panels/UserProfile.js
@@ -8,15 +8,20 @@ class UserProfile extends React.Component {
 			<div id="UserProfile">
 				<div className="title">Tensegrity</div>
 				<div className="container">
-					<button onClick={() => { this.props.onUpdatePanel('CardList')}} className="btn">Назад</button>
-					<div className="content-profile">
-						<img src={arrayCards[this.props.idUser].image} className="card-image"/>
-						<div className="card-info">
-							<div className="card-name">{arrayCards[this.props.idUser].name + ' ' + arrayCards[this.props.idUser].surname}</div>
-							<div className="card-phone">{arrayCards[this.props.idUser].phone}</div>
-							<div className="card-mail">{arrayCards[this.props.idUser].mail}</div>
+					<div className="sidebar">
+						<button onClick={() => { this.props.onUpdatePanel('CardList')}} className="btn">Назад</button>
+						<div className="content-profile">
+							<img src={arrayCards[this.props.idUser].image} className="card-image"/>
+							<div className="card-info">
+								<div className="card-name">{arrayCards[this.props.idUser].name + ' ' + arrayCards[this.props.idUser].surname}</div>
+								<div className="card-phone">{arrayCards[this.props.idUser].phone}</div>
+								<div className="card-mail">{arrayCards[this.props.idUser].mail}</div>
+							</div>
+							<button className="btn">Связаться</button>
 						</div>
-						<button className="btn">Связаться</button>
+					</div>
+					<div className="content">
+						Леддеры
 					</div>
 				</div>
 			</div>
