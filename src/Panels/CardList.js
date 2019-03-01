259fe1e32df493bb8b0fe25807b9674db1bdd5e7
Mike Petrov <mis5116@yandex.ru>
2019-03-01 04:04:17 +0300

    add new function checkbox

--- src/Panels/CardList.js
+++ src/Panels/CardList.js
@@ -6,11 +6,21 @@ class CardList extends React.Component {
 	constructor (props) {
         super(props);
         this.state = {
-			displayedCards: arrayCards
+			displayedCards: arrayCards,
+			skills: []
 		}
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleCheckbox = this.handleCheckbox.bind(this);
 	}
 
+	handleCheckbox(e) {
+		var searchQuery = e.toLowerCase();
+		var displayedCards = arrayCards.filter((el) => {
+			var searchValue = el.skills.toLowerCase();
+			return searchValue.indexOf(searchQuery) !== -1;
+		});
+		this.setState({ displayedCards: displayedCards});
+	}
 
 	handleSearch(e) {
 		var searchQuery = e.target.value.toLowerCase();
@@ -24,45 +34,48 @@ class CardList extends React.Component {
 	render() {
 		return (
 			<div id="cards">
-				<div className="title">HR 2.0</div>
+				<div className="title">Tensegrity</div>
 				<div className="container">
 					<div className="sidebar">
 						<input className="input" type="text" placeholder="Поиск" onChange={this.handleSearch}/>
 						<div className="steps-list">
+							<div className="subtitle">Экономика</div>
 							<div className="inputGroup">
-								<input id="option1" name="Макроэкономика" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Макроэкономика')}} id="option1" name="Макроэкономика" type="checkbox"/>
 								<label htmlFor="option1">Макроэкономика</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option2" name="Микроэкономика" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Микроэкономика')}} id="option2" name="Микроэкономика" type="checkbox"/>
 								<label htmlFor="option2">Микроэкономика</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option3" name="Гиперэкономика" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Гиперэкономика')}} id="option3" name="Гиперэкономика" type="checkbox"/>
 								<label htmlFor="option3">Гиперэкономика</label>
 							</div>
+							<div className="subtitle">Программирование</div>
 							<div className="inputGroup">
-								<input id="option4" name="JavaScript" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('JavaScript')}} id="option4" name="JavaScript" type="checkbox"/>
 								<label htmlFor="option4">JavaScript</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option5" name="React" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('React')}} id="option5" name="React" type="checkbox"/>
 								<label htmlFor="option5">React</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option6" name="Танцы с бубном" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Танцысбубном')}} id="option6" name="Танцысбубном" type="checkbox"/>
 								<label htmlFor="option6">Танцы с бубном</label>
 							</div>
+							<div className="subtitle">Хакатон</div>
 							<div className="inputGroup">
-								<input id="option7" name="Роутеризация" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Роутеризация')}} id="option7" name="Роутеризация" type="checkbox"/>
 								<label htmlFor="option7">Роутеризация</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option8" name="Набивание пуфиков" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Набиваниепуфиков')}} id="option8" name="Набиваниепуфиков" type="checkbox"/>
 								<label htmlFor="option8">Набивание пуфиков</label>
 							</div>
 							<div className="inputGroup">
-								<input id="option9" name="Мерч" type="checkbox"/>
+								<input onClick={() => {this.handleCheckbox('Мерч')}} id="option9" name="Мерч" type="checkbox"/>
 								<label htmlFor="option9">Мерч</label>
 							</div>
 						</div>
