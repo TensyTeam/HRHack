07fa2d9ea4d9af29a47e22cab14a942cc836dd52
w0ckeezz <39992389+w0ckeezz@users.noreply.github.com>
2019-03-01 04:59:39 +0300

    getHistory is fixed
    Take returns from functions using "obertka" functions

--- contractinteraction.js
+++ contractinteraction.js
@@ -7,7 +7,7 @@ function newEmployee(_name, _phone, _email) {
             if(error) {
                 return console.log(error);
             }
-            console.log("txhash: " + result);
+             console.log("txhash: " + result);
         }
     )
 
@@ -22,7 +22,7 @@ function changeEmployee(_newName, _newPhone, _newEmail) {
             if(error) {
                 return console.log(error);
             }
-            console.log("txhash: " + result);
+             console.log("txhash: " + result);
         }
     )
 
@@ -34,7 +34,6 @@ function getEmployee(_id) {
         if(error) {
             return console.log(error);
         }
-        console.log(result);
         return result; // - array of 3 strings
     });
 
@@ -56,59 +55,84 @@ function passStep(_id, _duration) {
 }
 
 function getNumberOfEmployees() {
-
-    contract.getNumberOfEmployees.call((error, result) => {
-        if(error) {
-            return console.log(error);
-        }
-        console.log(result.c[0]);
-        return result.c[0];
+    return new Promise(function(resolve, reject) {
+        contract.getNumberOfEmployees.call((error, result) => {
+            if(error) {
+                return console.log(error);
+            }
+            resolve(result.c[0]);
+        });
+    
     });
-
+    
 }
 
 function getNumberOfSteps(_employeeId) {
-
-    contract.getNumberOfSteps.call(_employeeId, (error, result) => {
-        if(error) {
-            return console.log(error);
-        }
-        console.log(result.c[0]);
-        return result.c[0];
+    return new Promise(function(resolve, reject) {
+        let la = contract.getNumberOfSteps.call(_employeeId, (error, result) => {
+            if(error) {
+                return console.log('la',error);
+            }
+            resolve(result.c[0]);
+        });
+    
     });
-
+    
 }
 
 function getEmployeeStep(_employeeId, _stepId) {
-
-    contract.getEmployeeStep.call(
-        _employeeId, _stepId,
-        (error, result) => {
-        if(error) {
-            return console.log(error);
-        }
-        console.log([result[0].c[0], result[1].c[0], result[2].c[0], result[3].c[0]]);
-        
-        return [result[0].c[0], result[1].c[0], result[2].c[0], result[3].c[0]]; // array of id, date, duration, solverId
+    return new Promise(function(resolve, reject) {
+        contract.getEmployeeStep.call(_employeeId, _stepId, (error, result) => {
+            if(error) {
+                return console.log(error);
+            }
+            resolve([result[0].c[0], result[1].c[0], result[2].c[0], result[3].c[0]]);
+        });
     });
-
+    
 }
 
-//do not work
 function getHistory() {
-
+    return new Promise(function(resolve, reject) {
     var history = [];
-    numOfEmployees =  getNumberOfEmployees(); // further code do no wait
-    console.log(numOfEmployees);
-    for (i = 0; i < numOfEmployees; i++) {
-        numOfSteps = getNumberOfSteps();
-        console.log('hui');
-        for(j = 0; j < numOfSteps; j++) {
-            var step = getEmployeeStep(i, j);
-            history.push(step);
-        }
+    getNumberOfEmployees().then(numOfEmployees =>
+        {
+            for (i = 0; i < numOfEmployees; i++) {
+                const bla = i;
+                getNumberOfSteps(i).then(numOfSteps =>
+                    {   
+                        for(j = 0; j < numOfSteps; j++) {
+                            getEmployeeStep(bla, j).then(step => {
+                                history.push(step);
+                                resolve(history); // array of all the steps
+                            })
+                            
+                        }
+                    })
+        
     }
-    console.log(history);
-    return history; // array of all the steps
 
+        }
+    )
+    });
 }
+
+// function obertkaGetEmployee() {
+//     getEmployee().then(x => console.log(x))
+// }
+
+// function obertkaGetNumberOfEmployees() {
+//     getNumberOfEmployees().then(x => console.log(x))
+// }
+
+// function obertkaGetNumberOfSteps() {
+//     getNumberOfSteps().then(x => console.log(x))
+// }
+
+// function obertkaGetEmployeeStep() {
+//     getEmployeeStep().then(x => console.log(x))
+// }
+
+// function obertkaGetHistory() {
+//     getHistory().then(x => console.log(x))
+// }
