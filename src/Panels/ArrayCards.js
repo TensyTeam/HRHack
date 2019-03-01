07d8f6eb1e7fbcd3b647051b41693a56d5f17167
Mike Petrov <mis5116@yandex.ru>
2019-03-01 03:52:42 +0300

    new structure

--- src/Panels/ArrayCards.js
+++ src/Panels/ArrayCards.js
@@ -4,40 +4,46 @@ export var arrayCards = [{
 	surname: 'Петров',
 	image: 'https://playground24.ru/img/mike.png',
 	phone: '89994307232',
-	mail: 'mike@mail.ru'
+	mail: 'mike@mail.ru',
+	skills: 'Макроэкономика, Микроэкономика, Гиперэкономика, React, Роутеризация, Набиваниепуфиков'
 }, {
 	id: 1,
 	name: 'Савва',
 	surname: 'Антонюк',
 	image: 'https://playground24.ru/img/savva.png',
 	phone: '89994357232',
-	mail: 'savva@mail.ru'
+	mail: 'savva@mail.ru',
+	skills: 'Мерч, Макроэкономика, Роутеризация, JavaScript, Микроэкономика, Макроэкономика'
 }, {
 	id: 2,
 	name: 'Алексей',
 	surname: 'Полоз',
 	image: 'https://playground24.ru/img/alex.png',
 	phone: '89434357542',
-	mail: 'poloz@mail.ru'
+	mail: 'poloz@mail.ru',
+	skills: 'Танцысбубном, Гиперэкономика, Макроэкономика'
 }, {
 	id: 3,
 	name: 'Леонид',
 	surname: 'Романычев',
 	image: 'https://playground24.ru/img/leonid.png',
 	phone: '89434357542',
-	mail: 'leoneed@mail.ru'
+	mail: 'leoneed@mail.ru',
+	skills: 'Роутеризация, JavaScript, Микроэкономика'
 }, {
 	id: 4,
 	name: 'Ксения',
 	surname: 'Манскова',
 	image: 'https://playground24.ru/img/ksenia.png',
 	phone: '89434357542',
-	mail: 'ksenia@mail.ru'
+	mail: 'ksenia@mail.ru',
+	skills: 'Мерч, Макроэкономика, Набиваниепуфиков, React, Микроэкономика'
 }, {
 	id: 5,
 	name: 'Сергей',
 	surname: 'Айхель',
 	image: 'https://playground24.ru/img/sergei.png',
 	phone: '89402357542',
-	mail: 'sergei@mail.ru'
+	mail: 'sergei@mail.ru',
+	skills: 'Макроэкономика, Гиперэкономика, JavaScript'
 }];
